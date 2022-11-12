69112282f256e619f5029b82e497fb675d0b8514
Ahmed Gomaa <a.gomaa003@gmail.com>
2022-11-12 14:19:05 +0200

    feat: finished architecture setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('tttttttttttttttttttttttt');
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json());
+
+const {PORT} = process.env;
+app.listen(PORT, () => {
+    console.log(`Server started on localhost:${PORT}`);
+});
+
+export default app;
