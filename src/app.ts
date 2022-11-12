33e4cfcb231dcaaa79aa93610dd3908d11fbc5f4
Ahmed Gomaa <a.gomaa003@gmail.com>
2022-11-12 15:50:30 +0200

    feat: Added port successfully

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,9 @@ dotenv.config();
 
 app.use(bodyParser.json());
 
-const {PORT} = process.env;
+const { PORT } = process.env;
 app.listen(PORT, () => {
-    console.log(`Server started on localhost:${PORT}`);
+  console.log(`Server started on localhost:${PORT}`);
 });
 
 export default app;
