b6e3be1ae06c5f3a37ccf8b459960c2b52da5de6
Ahmed Gomaa <a.gomaa003@gmail.com>
2022-11-14 13:29:58 +0200

    feat: Added product mappers

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,5 +1,38 @@
-export type Product = {
+import { ProductInfo } from "../dtos/product-info";
+
+export class Product {
   id: number;
   name: string;
   price: number;
+
+  static ConvertToProductInfo(source: Product): ProductInfo{
+    const result = new ProductInfo();
+    
+    result.id = source.id;
+    result.name = source.name;
+    result.price = source.price;
+
+    return result;
+    }
+
+  static ConvertFromProductInfo(source: ProductInfo): Product{
+      const result = new Product();
+      
+      result.id = source.id;
+      result.name = source.name;
+      result.price = source.price;
+  
+      return result;
+      }
+  
+  static ConvertToProductInfoList(source: Product[]): ProductInfo[]{
+    const result: ProductInfo[] = [];
+
+    source.forEach(element => {
+      const val = this.ConvertToProductInfo(element);
+      result.push(val);
+    });
+
+    return result;
+  }
 };
