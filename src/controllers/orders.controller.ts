32d9fe93a84cdd62b12402ec756c35fedb6548f8
Ahmed Gomaa <a.gomaa003@gmail.com>
2022-11-15 22:06:55 +0200

    feat: Added order service and controller

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,18 +1,47 @@
 import express, { Request, Response } from "express";
 import authorized from "../middlewares/check-jwt";
+import jwt from 'jsonwebtoken';
+import { OrdersService } from "../services/order.service";
+import { User } from "../models/user";
+import { JwtPayload } from "../dtos/jwt-payload";
+import { GeneralResponse, GeneralResponseList } from "../dtos/responses/general-responses";
+import { OrderDetails } from "../dtos/order-details";
 
-const getActiveOrders =  (req: Request, res: Response) => {
+const ordersService = new OrdersService();
+const getActiveOrders = async (req: Request, res: Response) => {
     try{
+        const token = req.headers.authorization?.split(' ')[1];
+        const payload = jwt.verify(token as string, process.env.JWT_SECRET as jwt.Secret) as JwtPayload;
+        const orders = await ordersService.getActiveOrders(payload.user.id);
+        if(!orders){
+            return res.status(400).send('Error');
+        }
 
+        const response = new GeneralResponseList<OrderDetails>();
+        response.data = orders;
+        response.count = orders.length;
+
+        res.json(response);
     }
     catch(err){
         return res.status(400).send('Error');
     }
 };
 
-const getClosedOrders =  (req: Request, res: Response) => {
+const getCompletedOrders =  async (req: Request, res: Response) => {
     try{
+        const token = req.headers.authorization?.split(' ')[1];
+        const payload = jwt.verify(token as string, process.env.JWT_SECRET as jwt.Secret) as JwtPayload;
+        const orders = await ordersService.getCompletedOrders(payload.user.id);
+        if(!orders){
+            return res.status(400).send('Error');
+        }
 
+        const response = new GeneralResponseList<OrderDetails>();
+        response.data = orders;
+        response.count = orders.length;
+
+        res.json(response);
     }
     catch(err){
         return res.status(400).send('Error');
@@ -21,7 +50,19 @@ const getClosedOrders =  (req: Request, res: Response) => {
 
  const placeOrders = (req: Request, res: Response) => {
     try{
+        const order = JSON.parse(JSON.stringify(req.body)) as OrderDetails;
+        const token = req.headers.authorization?.split(' ')[1];
+        const payload = jwt.verify(token as string, process.env.JWT_SECRET as jwt.Secret) as JwtPayload;
+        const returedOrder = ordersService.createNewOrder(payload.user.id, order.products);
 
+        if(!returedOrder){
+            return res.status(400).send('Error');
+        }
+
+        const response = new GeneralResponse();
+        response.data = returedOrder;
+
+        return res.json(response);
     }
     catch(err){
         return res.status(400).send('Error');
@@ -30,7 +71,12 @@ const getClosedOrders =  (req: Request, res: Response) => {
 
 const updateOrder =  (req: Request, res: Response) => {
     try{
+        const order = JSON.parse(JSON.stringify(req.body)) as OrderDetails;
+        const token = req.headers.authorization?.split(' ')[1];
+        const payload = jwt.verify(token as string, process.env.JWT_SECRET as jwt.Secret) as JwtPayload;
+        ordersService.updateExistingOrder(payload.user.id, order);
 
+        return res.status(200).send('');
     }
     catch(err){
         return res.status(400).send('Error');
@@ -39,7 +85,7 @@ const updateOrder =  (req: Request, res: Response) => {
 
 const orders_routes = (app: express.Application) => {
     app.get('/orders/active', authorized, getActiveOrders),
-    app.get('/orders/completed', authorized, getClosedOrders),
+    app.get('/orders/completed', authorized, getCompletedOrders),
     app.post('/orders', authorized, placeOrders),
     app.put('/orders', authorized, updateOrder)
 };
---
+++ src/services/order.service.ts
@@ -0,0 +1,129 @@
+import client from "../database";
+import { OrderDetails } from "../dtos/order-details";
+import { ProductOrderInfo } from "../dtos/product-order-info";
+import { Order } from "../models/order";
+
+export class OrdersService{
+    async getActiveOrders(userId: number): Promise<OrderDetails[] | null>{
+        try{
+        const conn = await client.connect();
+        let sql = `SELECT * FROM orders WHERE user_id = $1 and status = 'ACTIVE'`;
+        const result = await conn.query(sql, [userId]);
+        
+        const orders =  result.rows.map(x => {
+            const temp = new OrderDetails();
+            temp.id = x.id;
+            temp.status = x.status;
+            return temp;
+        }) ;
+        const returnOrders: OrderDetails[] = [];
+        await Promise.all(orders.map(async (order) => {
+            sql = `SELECT p.id AS id, p.name AS name, p.price AS price, po.quantity as quantity
+             FROM orders AS o
+             JOIN products_orders as po 
+             on o.id = po.order_id
+             JOIN products AS p
+             on po.product_id = p.id
+             WHERE o.id = $1;`;
+             const products = await conn.query(sql, [order.id]);
+             order.products = products.rows;
+             order.totalPrice = this.calculateOrderTotalPrice(order.products);
+             returnOrders.push(order);
+        }))
+        conn.release();
+        return returnOrders;
+        }
+        catch(err){
+            return null;
+        }
+    }
+
+    async getCompletedOrders(userId: number): Promise<OrderDetails[] | null>{
+        try{
+        const conn = await client.connect();
+        let sql = `SELECT * FROM orders WHERE user_id = $1 and status = 'COMPLETED'`;
+        const result = await conn.query(sql, [userId]);
+        
+        const orders =  result.rows.map(x => {
+            const temp = new OrderDetails();
+            temp.id = x.id;
+            temp.status = x.status;
+            return temp;
+        }) ;
+        const returnOrders: OrderDetails[] = [];
+        await Promise.all(orders.map(async (order) => {
+            sql = `SELECT p.id AS id, p.name AS name, p.price AS price, po.quantity as quantity
+             FROM orders AS o
+             JOIN products_orders as po 
+             on o.id = po.order_id
+             JOIN products AS p
+             on po.product_id = p.id
+             WHERE o.id = $1;`;
+             const products = await conn.query(sql, [order.id]);
+             order.products = products.rows;
+             order.totalPrice = this.calculateOrderTotalPrice(order.products);
+             returnOrders.push(order);
+        }));
+        conn.release();
+        return returnOrders;
+        }
+        catch(err){
+            return null;
+        }
+    }
+
+    async createNewOrder(userId: number, products: ProductOrderInfo[]): Promise<OrderDetails | null>{
+        try{
+            const conn = await client.connect();
+        let sql = `INSERT INTO orders(status, user_id) VALUES($1, $2) RETURNING*;`;
+        const result = await conn.query(sql, ['ACTIVE', userId]);
+
+        const order = new OrderDetails();
+        order.id = result.rows[0].id;
+        order.status = result.rows[0].status;
+
+        await Promise.all(products.map(async (product) => {
+            sql = `INSERT INTO products_orders(product_id, order_id, quantity)
+            VALUES($1, $2, $3) RETURNING*`;
+             const productsOrders = await conn.query(sql, [product.id, order.id, product.quantity]);
+        }));
+        return order;
+        }
+        catch(err){
+            return null;
+        }
+    }
+
+    async updateExistingOrder(userId: number, order: OrderDetails): Promise<void | null>{
+        try{
+            const conn = await client.connect();
+        let sql = `UPDATE orders set status = $1 WHERE id = $2 and user_id = $3 RETURNING*;`;
+        let result = await conn.query(sql, [order.status, order.id, userId]);
+
+        if(!result.rows[0]){
+            throw new Error('Order Dosn\'t exist');
+        }
+        sql = `DELETE FROM products_orders WHERE order_id = $1;`;
+        result = await conn.query(sql, [order.id]);
+
+        await Promise.all(order.products.map(async (product) => {
+            sql = `INSERT INTO products_orders(product_id, order_id, quantity)
+            VALUES($1, $2, $3) RETURNING*;`;
+             const productsOrders = await conn.query(sql, [product.id, order.id, product.quantity]);
+        }));
+        }
+        catch(err){
+            return null;
+        }
+    }
+    
+
+        private calculateOrderTotalPrice(products: ProductOrderInfo[]): number {
+            let sum = 0;
+            products.forEach(element => {
+                sum += element.price * element.quantity;
+            });
+
+            return sum;
+        }
+}
